983acb0ee425206547fa2b72aed7c9fb55cad0d2
Anthony <funnybunnyofdoom@aim.com>
2020-05-31 15:43:13 -0400

    Add BunnyUtilities
    Combine Simple Spawn and RTP and add permissions.

---
+++ bunnyutilities/src/BunnyUtilities.cs
@@ -0,0 +1,107 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vintagestory.API.Common;
+using Vintagestory.API.Common.Entities;
+using Vintagestory.API.MathTools;
+using Vintagestory.API.Server;
+using Vintagestory.GameContent;
+
+public class BunnyUtilities : ModSystem
+{
+	EntityPlayer GEntity;
+	int count = 0;
+	long LID;
+	ICoreServerAPI myAPI;
+
+	public override bool ShouldLoad(EnumAppSide side)
+	{
+		return side == EnumAppSide.Server;
+	}
+
+	public override void StartServerSide(ICoreServerAPI api)
+	{
+		base.StartServerSide(api); //Register the server api to "api"
+		IPermissionManager ipm = api.Permissions;
+		ipm.RegisterPrivilege("rtp","Random Teleport");
+		ipm.RegisterPrivilege("spawn", "Teleport to Spawn");
+
+
+		//Random Teleport
+
+		api.RegisterCommand("rtp", "Randomly Teleports the Player","",
+			(IServerPlayer player, int groupId, CmdArgs args) =>
+			{
+				EntityPlayer byEntity = player.Entity; //Get the player
+				GEntity = player.Entity;
+				IWorldManagerAPI world = api.WorldManager;
+				if (count == 0) 
+				{
+					player.SendMessage(Vintagestory.API.Config.GlobalConstants.GeneralChatGroup, "Teleporting to a random location", Vintagestory.API.Common.EnumChatType.Notification);
+					var randx = byEntity.World.Rand.Next(400000, 600000);//Using a hard coded 100000x in each direction until I can do the math
+					var randz = byEntity.World.Rand.Next(400000, 600000);
+					byEntity.Properties.FallDamage = false;
+					byEntity.TeleportTo(randx, 199, randz);
+					count = 1;
+					myAPI = api;
+					LID = api.Event.RegisterGameTickListener(OnTick, 15000); // register the tick listener
+				}
+				else
+				{
+					player.SendMessage(Vintagestory.API.Config.GlobalConstants.GeneralChatGroup, "Please wait a short while before trying again.", Vintagestory.API.Common.EnumChatType.Notification);
+				}
+
+				
+				
+			}, BPrivilege.rtp);
+
+		//Simple Spawn
+		api.RegisterCommand("spawn", "Teleports the player to spawn", "",
+			(IServerPlayer player, int groupId, CmdArgs args) =>
+			{
+				EntityPlayer byEntity = player.Entity; //Get the player
+
+
+				player.SendMessage(Vintagestory.API.Config.GlobalConstants.GeneralChatGroup, "You are teleporting to spawn", Vintagestory.API.Common.EnumChatType.Notification);
+				EntityPos spawnpoint = byEntity.World.DefaultSpawnPosition;
+				byEntity.TeleportTo(spawnpoint);
+
+
+			}, BPrivilege.spawn);
+		
+		//Rising Sun
+	}
+
+	private void OnTick(float dt)
+	{
+		if (count >= 3)
+		{
+			GEntity.Properties.FallDamage = true;
+			count = 0;
+			ICoreServerAPI api = myAPI;
+			myAPI.Event.UnregisterGameTickListener(LID);
+		}
+		else
+		{
+			count = count + 1;
+		}
+		Console.WriteLine(count);
+	}
+
+	public class BPrivilege : Privilege
+    {
+		/// <summary>
+		/// Ability to use /rtp
+		/// </summary>
+		public static string rtp = "rtp";
+
+		/// <summary>
+		/// Ability to use /spawn
+		/// </summary>
+		public static string spawn = "spawn";
+	}
+
+	
+}
