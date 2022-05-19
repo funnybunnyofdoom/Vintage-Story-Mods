ffb955f897e9fd510c17c5f11c65d48700f9bc6f
Anthony <funnybunnyofdoom@gmail.com>
2022-05-19 10:49:04 -0400

    Fixed cooldown timer, fixed rare fall death bug
    Fixed the cooldown timer. Before it stopped working until the server was restarted. It now lasts about 2 minutes. The fall damage now checks for "Feet on ground", "Feet in water" AND time since teleport. Now all three things are taken into account and should help reduce freak accidents.

--- JRTP/src/JustRandomTeleport.cs
+++ JRTP/src/JustRandomTeleport.cs
@@ -79,3 +79,86 @@ public class JustRandomTeleport : ModSystem
         public static string rtp = "rtp";
     }
 }
+using System;
+using Vintagestory.API.Common;
+using Vintagestory.API.Common.Entities;
+using Vintagestory.API.Server;
+//FunnyBunnyofDOOM@gmail.com
+//https://www.doomlandgaming.com
+
+public class JustRandomTeleport : ModSystem
+{
+    EntityPlayer GEntity;
+    int count = 0;
+    long LID,CID;
+    ICoreServerAPI myAPI;
+
+    public override bool ShouldLoad(EnumAppSide side)
+    {
+        return side == EnumAppSide.Server;
+    }
+
+    public override void StartServerSide(ICoreServerAPI api)
+    {
+        base.StartServerSide(api); //Register the server api to "api"
+        IPermissionManager ipm = api.Permissions;
+        ipm.RegisterPrivilege("rtp", "Random Teleport");
+        ipm.AddPrivilegeToGroup("suplayer", BPrivilege.rtp);
+
+        api.RegisterCommand("rtp", "Randomly Teleports the player", "",
+            (IServerPlayer player, int groupId, CmdArgs args) =>
+            {
+                EntityPlayer byEntity = player.Entity; //Get the player
+                GEntity = player.Entity;
+                IWorldManagerAPI world = api.WorldManager;
+                if (count == 0)
+                {
+                    player.SendMessage(Vintagestory.API.Config.GlobalConstants.GeneralChatGroup, "Teleporting to a random location", Vintagestory.API.Common.EnumChatType.Notification);
+                    var randx = byEntity.World.Rand.Next(400000, 600000);//Using a hard coded 100000x in each direction until I can do the math
+                    var randz = byEntity.World.Rand.Next(400000, 600000);
+                    byEntity.Properties.FallDamage = false;
+                    byEntity.TeleportTo(randx, 199, randz);
+                    count = 10;
+                    myAPI = api;
+                    LID = api.Event.RegisterGameTickListener(OnTick, 1000); // register the falling tick listener
+                    CID = api.Event.RegisterGameTickListener(CoolDown, 4500); // register the cooldown tick listener
+                }
+                else
+                {
+                    player.SendMessage(Vintagestory.API.Config.GlobalConstants.GeneralChatGroup, "Please wait a short while before trying again.", Vintagestory.API.Common.EnumChatType.Notification);
+                }
+            }, BPrivilege.rtp);
+    }
+
+    private void CoolDown(float ct)
+    {
+        if (count <= 0)
+        {
+            count = 0;
+            myAPI.Event.UnregisterGameTickListener(CID);
+        }
+        else
+        {
+            count = count - 1;
+        }
+    }
+    private void OnTick(float dt)
+    {     
+        if (GEntity.OnGround || GEntity.FeetInLiquid)
+        {
+            if (count <= 4){
+	    	GEntity.Properties.FallDamage = true;
+            	ICoreServerAPI api = myAPI;
+           	myAPI.Event.UnregisterGameTickListener(LID);
+		}
+        }
+    }
+
+    public class BPrivilege : Privilege
+    {
+        /// <summary>
+        /// Ability to use /rtp
+        /// </summary>
+        public static string rtp = "rtp";
+    }
+}
