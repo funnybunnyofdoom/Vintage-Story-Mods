3d377caf3999a82f32ee792479e7fc172f6e6a5d
Anthony <funnybunnyofdoom@gmail.com>
2022-06-17 18:45:15 -0400

    JRTP source update
    Updated the mod to wait for the chunk to load before teleporting the player

--- JRTP/src/JustRandomTeleport.cs
+++ JRTP/src/JustRandomTeleport.cs
@@ -2,15 +2,20 @@ using System;
 using Vintagestory.API.Common;
 using Vintagestory.API.Common.Entities;
 using Vintagestory.API.Server;
+using Vintagestory.API.Util;
+using Vintagestory.API.MathTools;
 //FunnyBunnyofDOOM@gmail.com
 //https://www.doomlandgaming.com
 
 public class JustRandomTeleport : ModSystem
 {
-    EntityPlayer GEntity;
-    int count = 0;
-    long LID,CID;
-    ICoreServerAPI myAPI;
+    public EntityPlayer GEntity;
+    public IServerPlayer Splayer;
+    public ICoreServerAPI myAPI;
+    public IServerChunk SChunk;
+    public BlockPos cblockpos;
+    int randx, randz = 0;
+
 
     public override bool ShouldLoad(EnumAppSide side)
     {
@@ -20,60 +25,46 @@ public class JustRandomTeleport : ModSystem
     public override void StartServerSide(ICoreServerAPI api)
     {
         base.StartServerSide(api); //Register the server api to "api"
+        myAPI = api;
         IPermissionManager ipm = api.Permissions;
         ipm.RegisterPrivilege("rtp", "Random Teleport");
         ipm.AddPrivilegeToGroup("suplayer", BPrivilege.rtp);
-
+        api.Event.ChunkColumnLoaded += OnChunkColumnLoaded;
         api.RegisterCommand("rtp", "Randomly Teleports the player", "",
-            (IServerPlayer player, int groupId, CmdArgs args) =>
+            cmd_rtp, BPrivilege.rtp);
+    }
+
+  
+
+    private void OnChunkColumnLoaded(Vec2i chunkCoord, IWorldChunk[] chunks)
+    {
+        if (randx / myAPI.WorldManager.ChunkSize == chunkCoord.X & (randz / myAPI.WorldManager.ChunkSize == chunkCoord.Y))
+        {
+            Splayer.SendMessage(Vintagestory.API.Config.GlobalConstants.GeneralChatGroup, "Teleporting to a random location.", Vintagestory.API.Common.EnumChatType.Notification);
+            int height = myAPI.World.BlockAccessor.GetRainMapHeightAt(randx,randz);
+            GEntity.TeleportTo(randx, height+1, randz);
+        }
+
+    }
+
+    private void cmd_rtp(IServerPlayer player, int groupId, CmdArgs args)
             {
-                EntityPlayer byEntity = player.Entity; //Get the player
-                GEntity = player.Entity;
-                IWorldManagerAPI world = api.WorldManager;
-                IWorldAccessor Aworld = api.World; //Add a world accessor to get our sea level. This should allow us to account for higher worlds
-                
+                ICoreServerAPI api = myAPI; //get the server api
+                Splayer = player;
+                GEntity = player.Entity; //assign the entity to global variable
+                IWorldManagerAPI world = api.WorldManager;             
                 if (count == 0)
-                {
-                    player.SendMessage(Vintagestory.API.Config.GlobalConstants.GeneralChatGroup, "Teleporting to a random location", Vintagestory.API.Common.EnumChatType.Notification);
-                    var randx = byEntity.World.Rand.Next(400000, 600000);//Using a hard coded 100000x in each direction until I can do the math
-                    var randz = byEntity.World.Rand.Next(400000, 600000);
-                    byEntity.Properties.FallDamage = false;
-                    byEntity.TeleportTo(randx, (Aworld.SeaLevel*2), randz); //Adding the sea level to 199 to get us 199 over sea level for high worlds
-                    count = 10;
-                    myAPI = api;
-                    LID = api.Event.RegisterGameTickListener(OnTick, 1000); // register the falling tick listener
-                    CID = api.Event.RegisterGameTickListener(CoolDown, 4500); // register the cooldown tick listener
+                {                   
+                    player.SendMessage(Vintagestory.API.Config.GlobalConstants.GeneralChatGroup, "Please wait while destination chunks are loaded.", Vintagestory.API.Common.EnumChatType.Notification);
+                    randx = GEntity.World.Rand.Next(400000, 600000);//Using a hard coded 100000x in each direction until I can do the math
+                    randz = GEntity.World.Rand.Next(400000, 600000);
+                    world.LoadChunkColumn(randx / myAPI.WorldManager.ChunkSize, randz / myAPI.WorldManager.ChunkSize);   
                 }
                 else
                 {
                     player.SendMessage(Vintagestory.API.Config.GlobalConstants.GeneralChatGroup, "Please wait a short while before trying again.", Vintagestory.API.Common.EnumChatType.Notification);
                 }
-            }, BPrivilege.rtp);
-    }
-
-    private void CoolDown(float ct)
-    {
-        if (count <= 0)
-        {
-            count = 0;
-            myAPI.Event.UnregisterGameTickListener(CID);
-        }
-        else
-        {
-            count = count - 1;
-        }
-    }
-    private void OnTick(float dt)
-    {     
-        if (GEntity.OnGround || GEntity.FeetInLiquid)
-        {
-            if (count <= 4){
-	    	GEntity.Properties.FallDamage = true;
-            	ICoreServerAPI api = myAPI;
-           	myAPI.Event.UnregisterGameTickListener(LID);
-		}
-        }
-    }
+            }
 
     public class BPrivilege : Privilege
     {
