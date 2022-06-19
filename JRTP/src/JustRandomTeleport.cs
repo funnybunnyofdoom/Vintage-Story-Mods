45b03cdc1fa3d2250ddfc613034e7b192c8cbb1d
Anthony <funnybunnyofdoom@gmail.com>
2022-06-19 14:08:44 -0400

    Bugfix - Cooldown and chunk priority
    Cooldown was broken (missing variable assignment) and chunk priority was changed to high in the chunk the player will be landing.

--- JRTP/src/JustRandomTeleport.cs
+++ JRTP/src/JustRandomTeleport.cs
@@ -48,6 +48,10 @@ public class JustRandomTeleport : ModSystem
             GEntity.TeleportTo(randx, height+1, randz);
             teleporting = false;
         }
+        if (teleporting == true)
+        {
+            Splayer.SendMessage(Vintagestory.API.Config.GlobalConstants.GeneralChatGroup, "loading chunk " + chunkCoord.X.ToString() + " " + chunkCoord.Y.ToString(), Vintagestory.API.Common.EnumChatType.Notification);
+        }
 
     }
 
@@ -56,17 +60,27 @@ public class JustRandomTeleport : ModSystem
                 ICoreServerAPI api = myAPI; //get the server api
                 Splayer = player;
                 GEntity = player.Entity; //assign the entity to global variable
-                IWorldManagerAPI world = api.WorldManager;             
-                if (count == 0)
+                IWorldManagerAPI world = api.WorldManager;
+                System.Diagnostics.Debug.Write(count);
+
+                if (count == 0 & teleporting == false)
                 {                   
                     player.SendMessage(Vintagestory.API.Config.GlobalConstants.GeneralChatGroup, "Please wait while destination chunks are loaded.", Vintagestory.API.Common.EnumChatType.Notification);
                     randx = GEntity.World.Rand.Next(400000, 600000);//Using a hard coded 100000x in each direction until I can do the math
                     randz = GEntity.World.Rand.Next(400000, 600000);
-                    world.LoadChunkColumn(randx / myAPI.WorldManager.ChunkSize, randz / myAPI.WorldManager.ChunkSize);
+                    world.LoadChunkColumnPriority(randx / myAPI.WorldManager.ChunkSize, randz / myAPI.WorldManager.ChunkSize);
+                   
+            {
+
+            }
                     CID = api.Event.RegisterGameTickListener(CoolDown, 4500); // register the cooldown tick listener
+                    count = 1;
                     teleporting = true;
-        }
-                else
+                }
+                else if(teleporting == true & count != 0){
+                    player.SendMessage(Vintagestory.API.Config.GlobalConstants.GeneralChatGroup, "Your chunks are still being generated, please be patient.", Vintagestory.API.Common.EnumChatType.Notification);
+                }
+        else
                 {
                     player.SendMessage(Vintagestory.API.Config.GlobalConstants.GeneralChatGroup, "Please wait a short while before trying again.", Vintagestory.API.Common.EnumChatType.Notification);
                 }
