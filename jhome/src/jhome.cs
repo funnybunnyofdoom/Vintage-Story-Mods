6086f83eb8ef5fe1ce83ee97045d4c72d8e5cfd4
Anthony <funnybunnyofdoom@gmail.com>
2022-06-22 20:44:49 -0400

    Add /back command to jhom
    added /back command and a toggle in configs to enable/disable /back command.

--- jhome/src/jhome.cs
+++ jhome/src/jhome.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -36,14 +36,18 @@ namespace jhome.src
             base.StartServerSide(api);
             sapi = api;
             IPermissionManager ipm = api.Permissions;
+            api.Event.PlayerDeath += OnPlayerDeath;
 
-            
+
             api.RegisterCommand("sethome", "Set your current position as home", " ",
                 cmd_sethome, BPrivilege.sethome);
             api.RegisterCommand("home", "Teleport to your /sethome location", " ",
                 cmd_home, CPrivilege.home);
+            api.RegisterCommand("back", "Go back to your last TP location", " ",
+                cmd_back, DPrivilege.back);
             ipm.RegisterPrivilege("sethome", "Set your current position as home",false);
             ipm.RegisterPrivilege("home", "Set your current position as home",false);
+            ipm.RegisterPrivilege("back", "Go back to your last TP location", false);
 
 
 
@@ -70,8 +74,12 @@ namespace jhome.src
             {
                 if (HomeConfig.Current.homeDict == null)
                     HomeConfig.Current.homeDict = HomeConfig.getDefault().homeDict;
-                if (HomeConfig.Current.enablePermissions== null)
+                if (HomeConfig.Current.backDict == null)
+                    HomeConfig.Current.backDict = HomeConfig.getDefault().backDict;
+                if (HomeConfig.Current.enablePermissions == null)
                     HomeConfig.Current.enablePermissions = HomeConfig.getDefault().enablePermissions;
+                if (HomeConfig.Current.enableBack == null)
+                    HomeConfig.Current.enableBack = HomeConfig.getDefault().enableBack;
 
                 api.StoreModConfig(HomeConfig.Current, "homeconfig.json");
             }
@@ -81,10 +89,51 @@ namespace jhome.src
                 ipm.AddPrivilegeToGroup("admin", CPrivilege.home);
                 ipm.AddPrivilegeToGroup("suplayer", CPrivilege.home);
                 ipm.AddPrivilegeToGroup("suplayer", BPrivilege.sethome);
+                ipm.AddPrivilegeToGroup("admin", DPrivilege.back);
+                ipm.AddPrivilegeToGroup("suplayer", DPrivilege.back);
             }
 
         }
 
+        private void OnPlayerDeath(IServerPlayer player, DamageSource damageSource)
+        {
+            if (HomeConfig.Current.enableBack == true)
+            {
+                if (HomeConfig.Current.backDict.ContainsKey(player.Entity.PlayerUID))
+                {
+                    HomeConfig.Current.backDict.Remove(player.Entity.PlayerUID);
+                }
+                HomeConfig.Current.backDict.Add(player.Entity.PlayerUID, player.Entity.Pos.AsBlockPos);
+                sapi.StoreModConfig(HomeConfig.Current, "homeconfig.json");
+            }
+        }
+
+        private void cmd_back(IServerPlayer player, int groupId, CmdArgs args)
+        {
+            
+            if (HomeConfig.Current.enableBack == true)
+            {
+                BlockPos newPos = player.Entity.Pos.AsBlockPos;
+                if (HomeConfig.Current.backDict.ContainsKey(player.Entity.PlayerUID))
+                {
+                    player.SendMessage(Vintagestory.API.Config.GlobalConstants.GeneralChatGroup, "Returning to your last location", Vintagestory.API.Common.EnumChatType.Notification);
+                    sapi.WorldManager.LoadChunkColumnPriority(HomeConfig.Current.backDict[player.Entity.PlayerUID].X / sapi.WorldManager.ChunkSize, HomeConfig.Current.backDict[player.Entity.PlayerUID].Z / sapi.WorldManager.ChunkSize);
+                    player.Entity.TeleportTo(HomeConfig.Current.backDict[player.Entity.PlayerUID].X, HomeConfig.Current.backDict[player.Entity.PlayerUID].Y, HomeConfig.Current.backDict[player.Entity.PlayerUID].Z);
+                    HomeConfig.Current.backDict.Remove(player.Entity.PlayerUID);
+                    HomeConfig.Current.backDict.Add(player.Entity.PlayerUID, newPos);
+                    sapi.StoreModConfig(HomeConfig.Current, "homeconfig.json");
+                }
+                else
+                {
+                    player.SendMessage(Vintagestory.API.Config.GlobalConstants.GeneralChatGroup, "No back location. Use /home to create a back location.", Vintagestory.API.Common.EnumChatType.Notification);
+                }
+            }
+            else
+            {
+                player.SendMessage(Vintagestory.API.Config.GlobalConstants.GeneralChatGroup, "Back is disabled. Check modconfigs/homeconfig.json to enable.", Vintagestory.API.Common.EnumChatType.Notification);
+            }
+            
+        }
 
         private void cmd_sethome(IServerPlayer player, int groupId, CmdArgs args)
         {
@@ -100,6 +149,15 @@ namespace jhome.src
         }
         private void cmd_home(IServerPlayer player, int groupId, CmdArgs args)
         {
+            if (HomeConfig.Current.enableBack == true)
+            {
+                if (HomeConfig.Current.backDict.ContainsKey(player.Entity.PlayerUID))
+                {
+                    HomeConfig.Current.backDict.Remove(player.Entity.PlayerUID);
+                }
+                HomeConfig.Current.backDict.Add(player.Entity.PlayerUID, player.Entity.Pos.AsBlockPos);
+                sapi.StoreModConfig(HomeConfig.Current, "homeconfig.json");
+            }
             if (HomeConfig.Current.homeDict.ContainsKey(player.Entity.PlayerUID))
             {
                 player.SendMessage(Vintagestory.API.Config.GlobalConstants.GeneralChatGroup, "Teleporting to your saved home", Vintagestory.API.Common.EnumChatType.Notification);
@@ -132,6 +190,15 @@ namespace jhome.src
             public static string home = "home";
 
         }
+        public class DPrivilege : Privilege
+        {
+            /// <summary>
+            /// Ability to use /sethome and /home
+            /// </summary>
+
+            public static string back = "back";
+
+        }
 
 
         public class HomeConfig
@@ -139,19 +206,30 @@ namespace jhome.src
             public static HomeConfig Current { get; set; }
 
             public Dictionary<String,BlockPos> homeDict { get; set; }
+            public Dictionary<String,BlockPos> backDict { get; set; }
             public bool? enablePermissions;
+            public bool? enableBack;
+
+
 
             public static HomeConfig getDefault()
             {
                 var config = new HomeConfig();
                 BlockPos defPos = new BlockPos(0,0,0);
                 bool perms = false;
-                Dictionary<String, BlockPos> homedictionary = new Dictionary<string, BlockPos>
+                bool backperms = true;
+                Dictionary<String, BlockPos> homedictionary = new Dictionary<string, BlockPos> 
+                {
+                    { "Default", defPos }
+                };
+                Dictionary<String, BlockPos> backdictionary = new Dictionary<string, BlockPos>
                 {
                     { "Default", defPos }
                 };
                 config.homeDict = homedictionary;
+                config.backDict = backdictionary;
                 config.enablePermissions = perms;
+                config.enableBack = backperms;
                 return config;
             }
 
