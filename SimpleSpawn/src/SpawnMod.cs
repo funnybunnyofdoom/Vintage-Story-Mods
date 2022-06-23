657061607d2d11ae279f00adfe48adf8fb91e981
Anthony <funnybunnyofdoom@gmail.com>
2022-06-23 10:20:58 -0400

    command method rewrite
    Made a new method for cmd_spawn

--- SimpleSpawn/src/SpawnMod.cs
+++ SimpleSpawn/src/SpawnMod.cs
@@ -22,17 +22,18 @@ public class SpawnMod : ModSystem
 	public override void StartServerSide(ICoreServerAPI api)
 	{
 		base.StartServerSide(api);
-		api.RegisterCommand("spawn", "Teleports the player to spawn", "",
-			(IServerPlayer player, int groupId, CmdArgs args) =>
-			{
-				EntityPlayer byEntity = player.Entity; //Get the player
+		api.RegisterCommand("spawn", "Teleports the player to spawn", "",cmd_spawn, Privilege.chat);
+	}
+
+    private void cmd_spawn(IServerPlayer player, int groupId, CmdArgs args)
+	{
+		EntityPlayer byEntity = player.Entity; //Get the player
 
 
-				player.SendMessage(Vintagestory.API.Config.GlobalConstants.GeneralChatGroup, "You are teleporting to spawn", Vintagestory.API.Common.EnumChatType.Notification);
-				EntityPos spawnpoint = byEntity.World.DefaultSpawnPosition;
-				byEntity.TeleportTo(spawnpoint);
+		player.SendMessage(Vintagestory.API.Config.GlobalConstants.GeneralChatGroup, "You are teleporting to spawn", Vintagestory.API.Common.EnumChatType.Notification);
+		EntityPos spawnpoint = byEntity.World.DefaultSpawnPosition;
+		byEntity.TeleportTo(spawnpoint);
 
 
-			}, Privilege.chat);
 	}
 }
