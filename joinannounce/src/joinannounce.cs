c7514a66bd919189d0a73fe778d2da716c9efadd
FunnyBunnyofDOOM <funnybunnyofdoom@gmail.com>
2022-07-12 10:05:40 -0400

    Add Join Announceq
    Mod will announce when a player joins for the first time

---
+++ joinannounce/src/joinannounce.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vintagestory.API.Common;
+using Vintagestory.API.Common.Entities;
+using Vintagestory.API.Datastructures;
+using Vintagestory.API.MathTools;
+using Vintagestory.API.Server;
+
+
+public class joinannounce : ModSystem
+{
+    ICoreServerAPI myAPI;
+    public override bool ShouldLoad(EnumAppSide side)
+    {
+        return side == EnumAppSide.Server;
+    }
+    public override void StartServerSide(ICoreServerAPI api)
+    {
+        base.StartServerSide(api);
+        myAPI = api;
+        api.Event.PlayerCreate += OnPlayerCreate;
+    }
+    public void OnPlayerCreate(IServerPlayer byPlayer)
+    {
+        myAPI.BroadcastMessageToAllGroups("Please welcome <font color=\"white\"><strong>" + byPlayer.PlayerName + "</strong></font> to the server!", Vintagestory.API.Common.EnumChatType.AllGroups);
+    }
+
+}
