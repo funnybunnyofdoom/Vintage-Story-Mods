0fff2c923732c0139dd36dcf835ae6007faf13df
FunnyBunnyofDOOM <funnybunnyofdoom@gmail.com>
2022-07-13 15:37:13 -0400

    JRTP cooldown config and help
    Added a /jrtp help command and
    /jrtp cooldown to configure the cooldown timer

--- JRTP/src/JustRandomTeleport.cs
+++ JRTP/src/JustRandomTeleport.cs
@@ -36,7 +36,7 @@ public class JustRandomTeleport : ModSystem
         ipm.RegisterPrivilege("rtp", "Random Teleport");
         ipm.AddPrivilegeToGroup("suplayer", BPrivilege.rtp);
         api.Event.ChunkColumnLoaded += OnChunkColumnLoaded;
-        api.RegisterCommand("rtp", "Randomly Teleports the player", "",
+        api.RegisterCommand("rtp", "Randomly Teleports the player", "[rtp|help|cooldown|version]",
             cmd_rtp, BPrivilege.rtp);
         cooldowntimer = 0;
         CID = api.Event.RegisterGameTickListener(CoolDown, 60000);
@@ -81,7 +81,6 @@ public class JustRandomTeleport : ModSystem
         if (randx / myAPI.WorldManager.ChunkSize == chunkCoord.X & (randz / myAPI.WorldManager.ChunkSize == chunkCoord.Y) & (teleporting == true))
         {
             Splayer.SendMessage(Vintagestory.API.Config.GlobalConstants.GeneralChatGroup, "Teleporting to a random location.", Vintagestory.API.Common.EnumChatType.Notification);
-            //int height = myAPI.World.BlockAccessor.GetRainMapHeightAt(randx,randz);
             BlockPos checkheight = new BlockPos();
             checkheight.X = randx;
             checkheight.Y = 1;
@@ -90,47 +89,85 @@ public class JustRandomTeleport : ModSystem
             GEntity.TeleportTo(randx, height+1, randz);
             teleporting = false;
         }
-        //if (teleporting == true)
-        //{
-         //   Splayer.SendMessage(Vintagestory.API.Config.GlobalConstants.GeneralChatGroup, "loading chunk " + chunkCoord.X.ToString() + " " + chunkCoord.Y.ToString(), Vintagestory.API.Common.EnumChatType.Notification);
-        //}
 
     }
 
     private void cmd_rtp(IServerPlayer player, int groupId, CmdArgs args)
-            {
+    {
+        string cmd = args.PopWord();
+        switch (cmd)
+        {
+            case "cooldown":
+                if (player.Role.Code == "admin") 
+                {
+                    int? cdnum = args.PopInt();
+                    if (cdnum == null)
+                    {
+                        player.SendMessage(Vintagestory.API.Config.GlobalConstants.GeneralChatGroup, "Please enter a number 0 or higher in minutes.", Vintagestory.API.Common.EnumChatType.Notification);
+                    }else if(cdnum < 0){
+                        player.SendMessage(Vintagestory.API.Config.GlobalConstants.GeneralChatGroup, "Please enter a non-negative number.", Vintagestory.API.Common.EnumChatType.Notification);
+                    }
+                    else
+                    {
+                        JrtpConfig.Current.cooldownduration = cdnum;
+                        myAPI.StoreModConfig(JrtpConfig.Current, "jrtpconfig.json");
+                    }
+                    //System.Diagnostics.Debug.WriteLine("PLAYer IS AN ADMIN");
+                }
+                else
+                {
+                    player.SendMessage(Vintagestory.API.Config.GlobalConstants.GeneralChatGroup, "You do not have permission to use this command.", Vintagestory.API.Common.EnumChatType.Notification);
+                }
+                break;
+            case "version":
+                var modinfo = Mod.Info;
+                player.SendMessage(Vintagestory.API.Config.GlobalConstants.GeneralChatGroup, "Mod Name: "+modinfo.Name + " | Author: FunnyBunnyofDOOM | Version: "+modinfo.Version, Vintagestory.API.Common.EnumChatType.Notification);
+                break;
+            case "help":
+                player.SendMessage(Vintagestory.API.Config.GlobalConstants.GeneralChatGroup, "RTP Commands:", Vintagestory.API.Common.EnumChatType.Notification);
+                player.SendMessage(Vintagestory.API.Config.GlobalConstants.GeneralChatGroup, "/rtp - Teleports the player to a random location", Vintagestory.API.Common.EnumChatType.Notification);
+                player.SendMessage(Vintagestory.API.Config.GlobalConstants.GeneralChatGroup, "/rtp version - Displays the version of the mod", Vintagestory.API.Common.EnumChatType.Notification);
+                if (player.Role.Code == "admin")
+                {
+                    player.SendMessage(Vintagestory.API.Config.GlobalConstants.GeneralChatGroup, "/rtp cooldown <i>number</i> - Sets the cooldown timer in minutes", Vintagestory.API.Common.EnumChatType.Notification);
+                }
+                break;
+            case null:
                 ICoreServerAPI api = myAPI; //get the server api
                 Splayer = player;
                 GEntity = player.Entity; //assign the entity to global variable
                 IWorldManagerAPI world = api.WorldManager;
                 System.Diagnostics.Debug.Write(count);
-
                 if (JrtpConfig.Current.cooldownDict.ContainsKey(player.PlayerUID) == false & teleporting == false)
-                {                   
+                {
                     player.SendMessage(Vintagestory.API.Config.GlobalConstants.GeneralChatGroup, "Please wait while destination chunks are loaded.", Vintagestory.API.Common.EnumChatType.Notification);
                     int radius = JrtpConfig.Current.teleportradius ?? default(int);
                     int worldx = world.MapSizeX;
                     int worldz = world.MapSizeZ;
-                    int rawxmin = (worldx/2) - radius;
+                    int rawxmin = (worldx / 2) - radius;
                     int rawxmax = (worldx / 2) + radius;
                     int rawzmin = (worldz / 2) - radius;
                     int rawzmax = (worldz / 2) + radius;
                     randx = GEntity.World.Rand.Next(rawxmin, rawxmax);
                     randz = GEntity.World.Rand.Next(rawzmin, rawzmax);
-                    world.LoadChunkColumnPriority(randx / myAPI.WorldManager.ChunkSize, randz / myAPI.WorldManager.ChunkSize);     
-                    count = 1;  
+                    world.LoadChunkColumnPriority(randx / myAPI.WorldManager.ChunkSize, randz / myAPI.WorldManager.ChunkSize);
+                    count = 1;
                     teleporting = true;
-                    JrtpConfig.Current.cooldownDict.Add(player.PlayerUID,cooldowntimer);
+                    JrtpConfig.Current.cooldownDict.Add(player.PlayerUID, cooldowntimer);
                     myAPI.StoreModConfig(JrtpConfig.Current, "jrtpconfig.json");
-        }
-                else if(teleporting == true){
+                }
+                else if (teleporting == true)
+                {
                     player.SendMessage(Vintagestory.API.Config.GlobalConstants.GeneralChatGroup, "Your chunks are still being generated, please be patient.", Vintagestory.API.Common.EnumChatType.Notification);
-                }else if(JrtpConfig.Current.cooldownDict.ContainsKey(player.PlayerUID) == true & teleporting == false)
+                }
+                else if (JrtpConfig.Current.cooldownDict.ContainsKey(player.PlayerUID) == true & teleporting == false)
                 {
                     int values;
-                    JrtpConfig.Current.cooldownDict.TryGetValue(player.PlayerUID,out values);
-                    player.SendMessage(Vintagestory.API.Config.GlobalConstants.GeneralChatGroup, "You can teleport again in " + ((values+JrtpConfig.Current.cooldownduration)-cooldowntimer) + " minutes", Vintagestory.API.Common.EnumChatType.Notification);
+                    JrtpConfig.Current.cooldownDict.TryGetValue(player.PlayerUID, out values);
+                    player.SendMessage(Vintagestory.API.Config.GlobalConstants.GeneralChatGroup, "You can teleport again in " + ((values + JrtpConfig.Current.cooldownduration) - cooldowntimer) + " minutes", Vintagestory.API.Common.EnumChatType.Notification);
                 }
+                break;
+        }
             }
 
     private void CoolDown(float ct)
@@ -141,11 +178,10 @@ public class JustRandomTeleport : ModSystem
             foreach (var keyvalue in tempdict)
             {
                 int value;
-                //var dic = JrtpConfig.Current.cooldownDict.Values;
+                
                 JrtpConfig.Current.cooldownDict.TryGetValue(keyvalue, out value);
                 if ((cooldowntimer - value) >= JrtpConfig.Current.cooldownduration)
                 {
-                    //myAPI.SendMessage(myAPI.World.PlayerByUid(keyvalue), Vintagestory.API.Config.GlobalConstants.GeneralChatGroup, "Please wait until the cooldown timer has elapsed", Vintagestory.API.Common.EnumChatType.Notification);
                     JrtpConfig.Current.cooldownDict.Remove(keyvalue);
                     myAPI.StoreModConfig(JrtpConfig.Current, "jrtpconfig.json");
                     return;
