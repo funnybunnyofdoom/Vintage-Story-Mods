af9b0101eb2297f8ed7a1c886bf4de632edcd50d
FunnyBunnyofDOOM <funnybunnyofdoom@gmail.com>
2022-07-19 15:35:21 -0400

    Add Mod - Bunny Bell
    Added a mod that makes a "ding" when a player mentions your name

---
+++ bunnybell/src/bunnybell.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vintagestory.API.Common;
+using Vintagestory.API.Datastructures;
+using Vintagestory.API.MathTools;
+using Vintagestory.API.Server;
+using Vintagestory.API.Util;
+
+namespace bunnybell.src
+{
+    class bunnybell : ModSystem
+    {
+        ICoreServerAPI sapi;
+        Dictionary<string, BlockPos> location;
+        AssetLocation sound = new AssetLocation("game", "sounds/effect/receptionbell");
+        public override void StartServerSide(ICoreServerAPI api)
+        {
+            base.StartServerSide(api);
+            sapi = api;
+            api.Event.PlayerChat += onPlayerChat;
+
+        }
+        private void onPlayerChat(IServerPlayer byPlayer, int channelId, ref string message, ref string data, BoolRef consumed)
+        {
+            string checklist = data;
+            IPlayer[] playerList = sapi.World.AllOnlinePlayers;
+            for (var i = 0; i < playerList.Count(); i++)
+            {
+                string templist = checklist;
+                IPlayer templayer = playerList[i];
+                if (templist.CaseInsensitiveContains(templayer.PlayerName))
+                {
+                    templayer.Entity.World.PlaySoundFor(sound,templayer);
+                }
+            }
+        }
+    }
+}
