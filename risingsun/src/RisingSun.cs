785dc2ce9ff318ab50efb259fc527610b1af3fa7
FunnyBunnyofDOOM <funnybunnyofdoom@gmail.com>
2022-07-20 16:30:12 -0400

    Rising Sun 1.0.2 - start
    added /rs help /rs version /rs dawn /rs dusk
    
    Added config file. WIP

--- risingsun/src/RisingSun.cs
+++ risingsun/src/RisingSun.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,29 +9,123 @@ using Vintagestory.API.Datastructures;
 using Vintagestory.API.MathTools;
 using Vintagestory.API.Server;
 
-
-public class RisingSun: ModSystem
+namespace risingsun.src
 {
-    public override bool ShouldLoad(EnumAppSide side)
+    class risingsun : ModSystem
     {
-        return side == EnumAppSide.Server;
-    }
-    public override void StartServerSide(ICoreServerAPI api)
-    {
-        base.StartServerSide(api);
-        api.Event.PlayerCreate += OnPlayerCreate;
-    } 
-        public void OnPlayerCreate(IServerPlayer byPlayer)
+        List<IServerPlayer> joinedPlayers = new List<IServerPlayer>();
+        ICoreServerAPI myAPI;
+        public override bool ShouldLoad(EnumAppSide side)
         {
-            byPlayer.SendMessage(Vintagestory.API.Config.GlobalConstants.GeneralChatGroup, "The sun has risen on a new player!", Vintagestory.API.Common.EnumChatType.AllGroups);
-            int hour = byPlayer.Entity.World.Calendar.FullHourOfDay;
-            if (hour < 8)
+            return side == EnumAppSide.Server;
+        }
+        public override void StartServerSide(ICoreServerAPI api)
+        {
+            base.StartServerSide(api);
+            myAPI = api;
+            api.Event.PlayerCreate += OnPlayerCreate;
+            api.Event.PlayerNowPlaying += onNowPlaying;
+
+            api.RegisterCommand("rs", "Rising Sun configuration", "[dawn|dusk|help|version]", cmd_rs, Privilege.controlserver);
+
+            try
             {
-            byPlayer.Entity.World.Calendar.Add(8 - hour);
-            }else if (hour > 8)
+                var Config = api.LoadModConfig<rsConfig>("rsconfig.json");
+                if (Config != null)
+                {
+                    api.Logger.Notification("Mod Config successfully loaded.");
+                    rsConfig.Current = Config;
+                }
+                else
+                {
+                    api.Logger.Notification("No Mod Config specified. Falling back to default settings");
+                    rsConfig.Current = rsConfig.getDefault();
+                }
+            }
+            catch
             {
-            byPlayer.Entity.World.Calendar.Add(24-hour+8);
+                rsConfig.Current = rsConfig.getDefault();
+                api.Logger.Error("Failed to load custom mod configuration. Falling back to default settings!");
+            }
+            finally
+            {
+
+                if (rsConfig.Current.dawn == null)
+                    rsConfig.Current.dawn = rsConfig.getDefault().dawn;
+                if (rsConfig.Current.dusk == null)
+                    rsConfig.Current.dusk = rsConfig.getDefault().dusk;
+
+                api.StoreModConfig(rsConfig.Current, "rsconfig.json");
             }
         }
-  
+
+        private void cmd_rs(IServerPlayer player, int groupId, CmdArgs args)
+        {
+            string cmd = args.PopWord();
+            switch (cmd)
+            {
+                case "help":
+                    displayhelp(player);
+                    break;
+                case "version":
+                    var modinfo = Mod.Info;
+                    player.SendMessage(Vintagestory.API.Config.GlobalConstants.GeneralChatGroup, "Mod Name: " + modinfo.Name + " | Author: FunnyBunnyofDOOM | Version: " + modinfo.Version, Vintagestory.API.Common.EnumChatType.Notification);
+                    break;
+                case "dawn":
+                    //Add configuration logic| not larger than dusk
+                    break;
+                case "dusk":
+                    //Add configuration logic
+                    break;
+                case null:
+                    player.SendMessage(Vintagestory.API.Config.GlobalConstants.GeneralChatGroup, "use /rs dawn|dusk|help|version", Vintagestory.API.Common.EnumChatType.Notification);
+                    break;
+            }
+        }
+
+        private void displayhelp(IServerPlayer player)
+        {
+            player.SendMessage(Vintagestory.API.Config.GlobalConstants.GeneralChatGroup, "rising sun Commands:", Vintagestory.API.Common.EnumChatType.Notification);
+            player.SendMessage(Vintagestory.API.Config.GlobalConstants.GeneralChatGroup, "/rs dawn <i>number</i> - Sets the hour Rising Sun will advance the night to", Vintagestory.API.Common.EnumChatType.Notification);
+            player.SendMessage(Vintagestory.API.Config.GlobalConstants.GeneralChatGroup, "/rs dusk <i>number</i> - Sets the hour that Rising Sun considers Night", Vintagestory.API.Common.EnumChatType.Notification);
+            player.SendMessage(Vintagestory.API.Config.GlobalConstants.GeneralChatGroup, "/home version - Displays the version information of Rising Sun", Vintagestory.API.Common.EnumChatType.Notification);
+        }
+
+        private void onNowPlaying(IServerPlayer byPlayer)
+        {
+            myAPI.BroadcastMessageToAllGroups("The sun has risen on a new player! Welcome " + byPlayer.PlayerName, Vintagestory.API.Common.EnumChatType.AllGroups);
+            int hour = byPlayer.Entity.World.Calendar.FullHourOfDay;
+            if (hour < rsConfig.Current.dawn)
+            {
+                
+                byPlayer.Entity.World.Calendar.Add((int)rsConfig.Current.dawn - hour);
+            }
+            else if (hour > (int)rsConfig.Current.dusk)
+            {
+                byPlayer.Entity.World.Calendar.Add(24 - hour + (int)rsConfig.Current.dawn);
+            }
+        }
+        public void OnPlayerCreate(IServerPlayer byPlayer)
+        {
+            joinedPlayers.Add(byPlayer);
+        }
+
+        public class rsConfig
+        {
+            public static rsConfig Current { get; set; }
+
+            public int? dawn { get; set; }
+            public int? dusk { get; set; }
+
+            public static rsConfig getDefault()
+            {
+                var config = new rsConfig();
+
+                config.dawn = 8;
+                config.dusk = 21;
+
+                return config;
+            }
+        }
+    }
 }
