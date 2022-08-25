a6acb167a71f391c5a0b7dcb2f9080ce877dcc1c
FunnyBunnyofDOOM <funnybunnyofdoom@gmail.com>
2022-08-25 18:17:22 -0400

    Update: Extra Juice Berry Milk
    Add BERRY MILK! Add metal mugs, goblets, and crafting recipes for all drinking objects.

---
+++ extrajuice/src/extrajuicecore.cs
@@ -0,0 +1,16 @@
+﻿
+using Vintagestory.API.Common;
+
+//Thanks to Craluminum for helping me to format my code mods like this
+
+namespace extrajuice.Load
+{
+    class extrajuice : ModSystem
+    {
+        public override void Start(ICoreAPI api)
+        {
+            base.Start(api);
+            api.World.Logger.Event("started 'Extra Juice' mod");
+        }
+    }
+}
