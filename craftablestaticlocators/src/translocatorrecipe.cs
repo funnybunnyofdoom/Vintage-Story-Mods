da09d004470a2f07caa436fe6800d6d666d60efc
FunnyBunnyofDOOM <funnybunnyofdoom@gmail.com>
2022-09-06 15:07:31 -0400

    Release: Craftable Static Translocators
    Add a recipe for static translocators

---
+++ craftablestaticlocators/src/translocatorrecipe.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vintagestory.API.Common;
+using Vintagestory.API.Server;
+
+namespace craftablestaticlocators.src
+{
+    class translocatorrecipe : ModSystem
+    {
+        ICoreServerAPI myAPI;
+        public override void StartServerSide(ICoreServerAPI api)
+        {
+            base.StartServerSide(api);
+            myAPI = api;
+        }
+
+    }
+}
