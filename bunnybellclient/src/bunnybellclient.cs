523d22c44c5abdca1e94cf04dc8d4ed7e5d14e69
FunnyBunnyofDOOM <funnybunnyofdoom@gmail.com>
2023-01-24 17:59:36 -0500

    Bunny Bell Client - Command and Sound
    Added the config sound and  the base command

--- bunnybellclient/src/bunnybellclient.cs
+++ bunnybellclient/src/bunnybellclient.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Vintagestory.API.Client;
 using Vintagestory.API.Common;
 using Vintagestory.API.Common.Entities;
+using Vintagestory.API.Config;
 using Vintagestory.API.Datastructures;
 using Vintagestory.API.MathTools;
 using Vintagestory.API.Util;
@@ -49,6 +50,7 @@ namespace bunnybellclient.src
             api.Event.PlayerJoin += onPlayerJoined; //Event Listener for when a player logs in
             api.Event.PlayerLeave += onPlayerLogout; //Event Listener for when a players logs out
             api.Event.OnEntityDeath += onPlayerDeath; // Event listener for when a player dies
+            api.RegisterCommand("bb", "Bunny Bell configuration", "[volume|mute|help|version]", cmd_bb);
 
             try
             {
@@ -79,32 +81,127 @@ namespace bunnybellclient.src
             }
         }
 
+        private void cmd_bb(int groupId, CmdArgs args)
+        {
+            IClientPlayer player = capi.World.Player;
+            string cmd = args.PopWord();
+            switch (cmd)
+            {
+                case "help":
+                    displayhelp(player);
+                    break;
+                case "version":
+                    var modinfo = Mod.Info;
+                    capi.World.Player.ShowChatNotification("Mod Name: " + modinfo.Name + " | Author: FunnyBunnyofDOOM | Version: " + modinfo.Version);
+                    break;
+                case "sound":
+                    var num = args.PopWord();
+                    if (num == null)
+                    {
+                        capi.World.Player.ShowChatNotification(Lang.Get("bunnybell:need-number", soundList.Count));
+                    }
+                    else
+                    {
+                        int result;
+                        if (int.TryParse(num, out result))
+                        {
+                            if (result > -1 && result <= soundList.Count() - 1)
+                            {
+                                capi.World.Player.ShowChatNotification(Lang.Get("bunnybell:play-sound", result));
+                                player.Entity.World.PlaySoundFor(soundList[result], player);
+                            }
+                            else
+                            {
+                                capi.World.Player.ShowChatNotification(Lang.Get("bunnybell:need-number", soundList.Count - 1));
+                            }
+                        }
+                        else
+                        {
+                            capi.World.Player.ShowChatNotification(Lang.Get("bunnybell:need-number", soundList.Count - 1));
+                        }
+                    }
+                    break;
+                case "set":
+                    string action = args.PopWord();
+                    int? sound = args.PopWord().ToInt();
+                    if (action == null || sound == null)
+                    {
+                        capi.World.Player.ShowChatNotification(Lang.Get("bunnybell:set-sound-help-1", soundList.Count - 1));
+                    }
+                    else if (sound < 0 || sound > soundList.Count - 1)
+                    {
+                        capi.World.Player.ShowChatNotification(Lang.Get("bunnybell:set-sound-help-2", soundList.Count - 1));
+                    }
+                    else
+                    {
+                            soundSettings tempSettings = bbConfig.Current.soundsettings;
+
+                            if (action == "mention")
+                            {
+                                tempSettings.mention = soundList[(int)sound];
+                                capi.World.Player.ShowChatNotification(Lang.Get("bunnybell:personal-sound-set", action, sound));
+                            }
+                            else if (action == "login")
+                            {
+                                tempSettings.login = soundList[(int)sound];
+                                capi.World.Player.ShowChatNotification(Lang.Get("bunnybell:personal-sound-set", action, sound));
+                            }
+                            else if (action == "logout")
+                            {
+                                tempSettings.logout = soundList[(int)sound];
+                                capi.World.Player.ShowChatNotification(Lang.Get("bunnybell:personal-sound-set", action, sound));
+                            }
+                            else if (action == "death")
+                            {
+                                tempSettings.death = soundList[(int)sound];
+                                capi.World.Player.ShowChatNotification(Lang.Get("bunnybell:personal-sound-set", action, sound));
+                            }
+                            else if (action == "PVPdeath")
+                            {
+                                tempSettings.PVPdeath = soundList[(int)sound];
+                                capi.World.Player.ShowChatNotification(Lang.Get("bunnybell:personal-sound-set", action, sound));
+                            }
+                            capi.StoreModConfig(bbConfig.Current, "bbclient.json");
+                    }
+                    break;
+            }
+        }
+
         private void onPlayerDeath(Entity entity, DamageSource damageSource)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
         }
 
         private void onPlayerLogout(IClientPlayer byPlayer)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
         }
 
         private void onPlayerJoined(IClientPlayer byPlayer)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
         }
 
         private void onPlayerChat(int groupId, string message, EnumChatType chattype, string data)
         {
-            int volume = 1; //this doesn't seem to work, leaving it in so I can try to adjust volume in the future
             IPlayer templayer = capi.World.Player; //Get the player
             if (templayer != null && templayer.PlayerName != null && data != null)//Make sure player is loaded and message is from a player
             {
                 if (data.CaseInsensitiveContains(templayer.PlayerName)) //Check for the players name within the message
                 {
-                    templayer.Entity.World.PlaySoundFor(sound, templayer, true, 32, volume);//Play a sound for the player
+                    templayer.Entity.World.PlaySoundFor(bbConfig.Current.soundsettings.mention, templayer, true, 32);//Play a sound for the player
                 }
             }
+        }
+
+        private void displayhelp(IClientPlayer player)
+        {
+            capi.World.Player.ShowChatNotification(Lang.Get("bunnybell:help-title"));
+            capi.World.Player.ShowChatNotification(Lang.Get("bunnybell:help-sound", soundList.Count - 1));
+            capi.World.Player.ShowChatNotification(Lang.Get("bunnybell:help-set", soundList.Count - 1));
+            capi.World.Player.ShowChatNotification(Lang.Get("bunnybell:help-enable"));
+            capi.World.Player.ShowChatNotification(Lang.Get("bunnybell:help-disable"));
+            capi.World.Player.ShowChatNotification(Lang.Get("bunnybell:help-version"));
         }
         public class soundSettings
         {
