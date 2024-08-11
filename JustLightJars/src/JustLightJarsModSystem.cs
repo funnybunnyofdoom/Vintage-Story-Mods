362c3a8b007ef4c77690ab2110aab4a02b3de581
FunnyBunnyofDOOM <funnybunnyofdoom@gmail.com>
2024-08-11 12:24:54 -0400

    Just Light Jars - 2.0.0
    -Fix small jars light when placed
    -Jars make light in hands
    -Compatible with 1.19.8

---
+++ JustLightJars/src/JustLightJarsModSystem.cs
@@ -0,0 +1,30 @@
+﻿using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Config;
+using Vintagestory.API.Server;
+using Vintagestory.API.Common;
+using Vintagestory.API.MathTools;
+using System;
+
+namespace JustLightJars
+{
+    public class JustLightJarsModSystem : ModSystem
+    {
+        public override void Start(ICoreAPI api)
+        {
+            base.Start(api);
+        }
+
+        public override void StartServerSide(ICoreServerAPI api)
+        {
+
+        }
+
+        public override void StartClientSide(ICoreClientAPI api)
+        {
+
+        }
+    }
+
+    
+}
